838cc20ad36a9b657b995b6ee438b8bb2982a98c
Wlad Neto <wladimirteixeira@yahoo.com.br>
2024-02-13 23:12:55 -0300

    feat: add addTransaction rules

--- api/src/api/clientes/clientes.handlers.ts
+++ api/src/api/clientes/clientes.handlers.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { ClienteWithId, Clientes } from './clientes.model';
-import { Transacao, TransacaoWithId, Transacoes } from '../transacoes/transacoes.model';
+import { Transacao } from '../transacoes/transacoes.model';
+import MessageResponse from '../../interfaces/MessageResponse';
+import TransactionResponse from '../../interfaces/TransactionResponse';
+
+type ClientTransactionResponse = TransactionResponse | MessageResponse;
 
 export async function findAll(req: Request, res: Response<ClienteWithId[]>, next: NextFunction) {
     try {
@@ -12,17 +16,53 @@ export async function findAll(req: Request, res: Response<ClienteWithId[]>, next
     }
 }
 
-export async function addTransaction(req: Request, res: Response<TransacaoWithId>, next: NextFunction) {
+export async function addTransaction(req: Request, res: Response<ClientTransactionResponse>, next: NextFunction) {
     try {
-        const userId = req.params.id;
-        const insertResult = await Transacoes.insertOne(req.body);
-        if (!insertResult.acknowledged) throw new Error('Erro ao inserir transacao.');
-        res.status(201);
+        let clientid = req.params.id;
+        const transaction: Transacao = req.body;
+        const client = await Clientes.findOne({ clienteid: parseInt(clientid) });
+        // If client doesn't exist, send an error response
+        if (!client) {
+            return res.status(404).json({ 'message': 'Cliente not found' }).send();
+        }
+
+        let resultTransaction = processTransaction(client.limite, client.saldo, transaction.tipo, transaction.valor);
+        //When resultTransaction is string account limit was exceeds.
+        if (typeof resultTransaction === 'string') {
+            return res.status(422).json({ 'message': resultTransaction }).send();
+        }
+
+        const date = new Date();
+        transaction.realizada_em = date.toISOString();
+        client.transacoes.push(transaction);
+
+        // Update the client's record in the database
+        await Clientes.updateOne(
+            { _id: client._id },
+            { $set: { saldo: resultTransaction, transacoes: client.transacoes } }
+        );
+
+        res.status(200);
         res.json({
-            _id: insertResult.insertedId,
-            ...req.body,
-        });
+            'limite': client.limite,
+            'saldo': resultTransaction
+        })
+
     } catch (error) {
         next(error);
     }
+}
+
+function processTransaction(limit: number, balance: number, transactionType: string, transactionValue: number) {
+    // Debit
+    if (transactionType === 'd') {
+        let newBalance: number;
+        newBalance = balance - transactionValue;
+        if (newBalance < -limit) {
+            return 'Error: Transaction exceeds account limit.';
+        }
+        return newBalance;
+    }
+    // Credit
+    return balance + transactionValue;
 }
