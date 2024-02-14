3c87c95128fd12227d6a2fea87dbb954b80837dc
Wlad Neto <wladimirteixeira@yahoo.com.br>
2024-02-13 23:08:45 -0300

    feat: remove transaction references resources

--- api/src/api/index.ts
+++ api/src/api/index.ts
@@ -1,11 +1,8 @@
 import express from 'express';
-
 import clientes from './clientes/clientes.routes'
-import transacoes from './transacoes/transacoes.routes'
 
 const router = express.Router();
 
 router.use('/clientes', clientes);
-router.use('/transacoes', transacoes); //TO:DO Remover
 
 export default router;
