32b62c3cdf8eedf721a104d8d610784c80823d8f
Wlad Neto <wladimirteixeira@yahoo.com.br>
2024-03-01 21:47:48 -0300

    fix: try another config

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import cors from 'cors';
 
 import * as middlewares from './middlewares';
 import api from './api';
@@ -13,7 +12,6 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-//app.use(cors());
 app.use(express.json());
 
 app.get<{}, MessageResponse>('/', (req, res) => {
